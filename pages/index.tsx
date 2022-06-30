0c9b390ce5ff64451467d2dc9149ce1f5fbfa50e
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2022-06-30 16:40:57 +0700

    added note

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
-// Inspired by: https://thegamefanatics.com/
-// and https://genshin.hoyoverse.com/en/news/
+// Inspired by: 
+// https://thegamefanatics.com/
+// https://genshin.hoyoverse.com/en/news/
+// https://cdnb.artstation.com/p/assets/images/images/035/116/599/large/woojin-lee-uiasset.jpg?1614141771
 
 const Home: NextPage = () => {
   return (
